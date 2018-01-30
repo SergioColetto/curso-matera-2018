38f1fb73c3ba4504157156765681b4b13300da5d
Sergio Coletto <sergiocoletto@me.com>
2018-01-30 19:12:36 -0200

    done step-03

--- src/test/java/matera/systems/cursoferias2018/api/resources/DisciplinasResourceIT.java
+++ src/test/java/matera/systems/cursoferias2018/api/resources/DisciplinasResourceIT.java
@@ -3,8 +3,13 @@ package matera.systems.cursoferias2018.api.resources;
 import io.restassured.RestAssured;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
+import matera.systems.cursoferias2018.api.domain.request.AtualizarDisciplinaRequest;
+import matera.systems.cursoferias2018.api.domain.request.CriaDisciplinaRequest;
+import matera.systems.cursoferias2018.api.domain.response.DisciplinaResponse;
 import matera.systems.cursoferias2018.api.domain.response.UsuarioResponse;
+import matera.systems.cursoferias2018.api.repository.DisciplinasRepositoryStub;
 import matera.systems.cursoferias2018.api.repository.UsuarioRepositoryStub;
+
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
@@ -43,8 +48,8 @@ public class DisciplinasResourceIT {
     @Test
     public void atualizaDisciplina() {
 
-        AtualizarDisciplinaRequest atualizarUsuarioRequest = new AtualizarDisciplinaRequest();
-        atualizarUsuarioRequest.descricao("Descricao");
+        AtualizarDisciplinaRequest atualizarDisciplinaRequest = new AtualizarDisciplinaRequest();
+        atualizarDisciplinaRequest.setDescricao("Descricao");
 
         Response response =
                 RestAssured
@@ -52,7 +57,7 @@ public class DisciplinasResourceIT {
                         .header(getAuthorizationHeader())
                         .header("Accept", "application/json")
                         .header("Content-Type", "application/json")
-                        .body(atualizarUsuarioRequest)
+                        .body(atualizarDisciplinaRequest)
                     .put(DISCIPLINAS_URL + "/" + DisciplinasRepositoryStub.DISCIPLINA_3.toString())
                         .thenReturn();
 
@@ -112,11 +117,11 @@ public class DisciplinasResourceIT {
     public void criarDisciplina() {
 
         CriaDisciplinaRequest createRequest = new CriaDisciplinaRequest();
-        createRequest.descricao("Back-end Java");
-        createRequest.dataInicio("29/01/2018");
-        createRequest.dataTermino("29/01/2018");
-        createRequest.segmento("Backend");
-        createRequest.urlLogo("http://pictures.pic/course1");
+        createRequest.setDescricao("Back-end Java");
+        createRequest.setDataInicio("29/01/2018");
+        createRequest.setDataTermino("29/01/2018");
+        createRequest.setSegmento("Backend");
+        createRequest.setUrlLogo("http://pictures.pic/course1");
 
         Response response =
                 RestAssured
